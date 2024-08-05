779ff585d2f6431174de9b97745345b56662315d
cristianManco <camilomanco2005@gmail.com>
2024-08-05 07:27:55 -0500

    feat: established database connection and created persistence module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PersistenceModule } from './Libs/persistence/persistence.module';
+import dbConfig from './Libs/persistence/db-config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+  ],
   controllers: [],
   providers: [],
 })
