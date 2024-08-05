4c6cdba13e17bb4f3e753885e77709a56d76ed11
cristianManco <camilomanco2005@gmail.com>
2024-08-05 07:33:02 -0500

    feat: established Swagger connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { AllExceptionsFilter } from './lib/common/filters/all-exceptions.filter';
+// import { ErrorsInterceptor } from './lib/common/interceptors/errors.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 3000;
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalFilters(new AllExceptionsFilter());
+  // app.useGlobalInterceptors(new ErrorsInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Riwi Centinela')
+    .setDescription(
+      'API for managing authentication in Riwi training center projects',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
